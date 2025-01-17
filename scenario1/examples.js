a51e8716d54aa4ea2167752027af6e13f705d83e
Edward Misback <misback@cs.washington.edu>
2025-01-17 12:58:28 -0800

    Add a bit to scenario 1.

--- scenario1/examples.js
+++ scenario1/examples.js
@@ -4,11 +4,19 @@ const testPayment = {
     currency: 'usd',
     payment_method: 'card',
     card: {
-        number: '4242424242424242',
+        number: '4242424242424242', // Test working card number
         exp_month: 12,
         exp_year: 2024,
         cvc: '123'
     }
+};
+
+// Response
+const successfulPaymentResponse = {
+    id: 'ch_1J2e5e2eZvKYlo2C9ZJ7L8e2',
+    amount: 2000,
+    currency: 'usd',
+    status: 'succeeded'
 };
 
 // Test payment declined
@@ -17,7 +25,7 @@ const declinedPayment = {
     currency: 'usd',
     payment_method: 'card',
     card: {
-        number: '4000000000000002',
+        number: '4000000000000002', // Test declined card number
         exp_month: 12,
         exp_year: 2024,
         cvc: '123'
